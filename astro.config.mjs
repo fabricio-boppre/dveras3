b3b2025c796c8d715aa9367fb99a93e4a9761e71
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-10-02 17:33:04 -0300

    Astro Content Devtools installed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
+  integrations: [solid()],
 });
