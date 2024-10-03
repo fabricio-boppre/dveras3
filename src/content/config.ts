d2254d122d10cb654cbb14bf994d7eaa6815d4ec
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-10-03 14:19:01 -0300

    Prettier installed, configured and executed.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Importing utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content"
 
 // 2. Defining our collection(s)
 const blogCollection = defineCollection({
@@ -10,10 +10,10 @@ const blogCollection = defineCollection({
     data_publicacao: z.date(),
     offline: z.boolean().optional(),
   }),
-});
+})
 
 // 3. Exporting a single `collections` object to register our collection(s)
 //    These keys should match our collections directories names in "src/content"
 export const collections = {
   blog: blogCollection,
-};
+}
