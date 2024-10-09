68b8ea14191ac5ebaf0e4f82396d5c3bcc23fd3a
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-10-09 17:29:15 -0300

    Packages updates.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from "astro/config";
-import solid from "@astrojs/solid-js";
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
-  integrations: [solid()],
-});
+})
