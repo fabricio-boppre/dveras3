0b5ee9d16f312a39e19957186a89560bc52f77eb
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-11-18 09:59:51 -0300

    RSS configured and footer finished.

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
+
+export async function GET(context) {
+  const posts = await getCollection("blog", ({ data }) => {
+    return data.offline !== true
+  })
+  return rss({
+    title: "dveras em rede",
+    description: "Blog de Dauro Veras.",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.titulo,
+      pubDate: post.data.data_publicacao,
+      description: post.data.descricao,
+      link: `/blog/${post.slug}/`,
+      customData: `<language>pt-br</language>`,
+    })),
+  })
+}
