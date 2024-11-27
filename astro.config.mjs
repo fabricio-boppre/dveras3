6b50631a9e9824341d1c0a63da67d623fec5fd4a
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-11-27 18:33:14 -0300

    Packages installed and updated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config"
 
+import metaTags from "astro-meta-tags";
+
+import pageInsight from "astro-page-insight";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
+  integrations: [metaTags(), pageInsight()],
 })
