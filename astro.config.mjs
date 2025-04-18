bc10563b1ca67614fce67440bc2da113dbfc54df
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-04-18 09:03:56 -0300

    Vite config added.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config"
 
-import metaTags from "astro-meta-tags";
+import metaTags from "astro-meta-tags"
 
-import pageInsight from "astro-page-insight";
+import pageInsight from "astro-page-insight"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
   integrations: [metaTags(), pageInsight()],
+  vite: { server: { allowedHosts: ["macleod.local"] } },
 })
