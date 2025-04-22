73d32fcc1691e9cc0c3900e6706a485c15df140b
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-04-22 16:24:26 -0300

    Astro Simap installed and configured.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import metaTags from "astro-meta-tags"
 
 import pageInsight from "astro-page-insight"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
-  integrations: [metaTags(), pageInsight()],
+  integrations: [metaTags(), pageInsight(), sitemap()],
   vite: { server: { allowedHosts: ["macleod.local"] } },
 })
