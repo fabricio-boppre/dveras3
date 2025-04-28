1dc1f0d0b4ff356ae160bdbd5271083d4feca459
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-04-28 17:15:15 -0300

    Installed integration Block AI bots.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import pageInsight from "astro-page-insight"
 
 import sitemap from "@astrojs/sitemap"
 
+import aiRobotsTxt from "astro-ai-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
-  integrations: [metaTags(), pageInsight(), sitemap()],
+  integrations: [metaTags(), pageInsight(), sitemap(), aiRobotsTxt()],
   vite: { server: { allowedHosts: ["macleod.local"] } },
 })
