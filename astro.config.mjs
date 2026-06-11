c1b951cd61a5e59c7e0c5c12e373ba3affe9be7f
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2026-06-11 17:00:17 -0300

    Updating the packages after project pause.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, fontProviders } from "astro/config"
 import metaTags from "astro-meta-tags"
-import pageInsight from "astro-page-insight"
 import sitemap from "@astrojs/sitemap"
 import aiRobotsTxt from "astro-ai-robots-txt"
 import remarkYoutube from "remark-youtube"
@@ -8,18 +7,17 @@ import remarkYoutube from "remark-youtube"
 // https://astro.build/config
 export default defineConfig({
   site: "https://v3.dauroveras.com.br",
-  integrations: [metaTags(), pageInsight(), sitemap(), aiRobotsTxt()],
+  integrations: [metaTags(), sitemap(), aiRobotsTxt()],
   markdown: {
     remarkPlugins: [remarkYoutube],
   },
   vite: { server: { allowedHosts: ["macleod.local"] } },
-  experimental: {
     fonts: [
       {
         provider: fontProviders.fontsource(),
         name: "Roboto Serif",
         cssVariable: "--font-roboto-serif",
-        weights: ["600", "700"],
+        weights: [600, 700],
         fallbacks: ["serif"],
       },
       {
@@ -29,5 +27,4 @@ export default defineConfig({
         fallbacks: ["sans-serif"],
       },
     ],
-  },
 })
---
+++ src/content/blog/testando-apos-longo-tempo.md
@@ -0,0 +1,12 @@
+---
+titulo: 'Testando, após longo tempo!'
+descricao: Um teste com capa de disco.
+data_publicacao: 2026-06-11T19:12:04.680Z
+lastUpdated: 2026-06-11T19:12:31.241Z
+modo_exibicao_index: imagem_ilustrativa
+imagem_ilustrativa: /images/content/frontal.jpg
+relatedCategorias:
+  - musica
+---
+
+Grande Cecil Taylor!
